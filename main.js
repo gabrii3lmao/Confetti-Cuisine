f66bde4e38b61a9a551f7221fecb4c35edcdc23f
gabrii3l <gabrielmluz56@gmail.com>
2025-12-31 09:26:02 -0300

    creating routes.js and updating controllers

--- main.js
+++ main.js
@@ -1,52 +1,23 @@
 const express = require("express");
 const app = express();
-const homeController = require("./controller/homeController");
 const errorController = require("./controller/errorController");
-const subscribersControllers = require("./controller/subscribeController");
+const router = require("./routes");
 
 const layouts = require("express-ejs-layouts");
 
 // mongoDb config
 const mongoose = require("mongoose");
-const courserController = require("./controller/courserController");
 
 // middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(layouts);
-
 //configs
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
 
-// rotas
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/createCourse", homeController.showCourseForm);
-app.post("/createCourse", courserController.createCourse, (req, res) => {
-  res.render("thanks");
-});
-
-app.get("/courses", courserController.getAllCourses, (req, res) => {
-  res.render("courses", { offeredCourses: req.data });
-});
-
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", subscribersControllers.createSubscriber, (req, res) => {
-  res.render("thanks");
-});
-
-app.get(
-  "/subscribers",
-  subscribersControllers.getAllSubscribers,
-  (req, res, next) => {
-    res.render("subscribers", { subscribers: req.data });
-  }
-);
-
+app.use(router)
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
@@ -61,7 +32,7 @@ async function startApplication() {
       );
     });
   } catch (error) {
-    console.error("Erro ao conectar no MongoDB:", err);
+    console.error("Erro ao conectar no MongoDB:", error);
   }
 }
 
---
+++ routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const homeController = require("./controller/homeController");
+const subscribersControllers = require("./controller/subscribeController");
+const courseController = require("./controller/courserController");
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/createCourse", homeController.showCourseForm);
+router.post("/createCourse", courseController.createCourse, (req, res) => {
+  res.render("thanks");
+});
+
+router.get("/courses", courseController.getAllCourses, (req, res) => {
+  res.render("courses", { offeredCourses: req.data });
+});
+
+router.get("/contact", homeController.showSignUp);
+router.post("/contact", subscribersControllers.createSubscriber, (req, res) => {
+  res.render("thanks");
+});
+
+router.get(
+  "/subscribers",
+  subscribersControllers.getAllSubscribers,
+  (req, res, next) => {
+    res.render("subscribers", { subscribers: req.data });
+  }
+);
+
+module.exports = router;
